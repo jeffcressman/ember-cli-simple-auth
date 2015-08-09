73bdc61510307ebadf55186d46a148d5c74cc828
Jeff Cressman <jeff.cressman@gmail.com>
2015-08-09 21:19:14 +0930

    Install ember-simple-auth from forked repo

--- blueprints/ember-cli-simple-auth/index.js
+++ blueprints/ember-cli-simple-auth/index.js
@@ -3,6 +3,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('ember-simple-auth', '0.8.0');
+    return this.addBowerPackageToProject('ember-cli-simple-auth', 'git://github.com/jeffcressman/ember-cli-simple-auth.git');
   }
 };
